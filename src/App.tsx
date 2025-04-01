72d996c870a1d2110fb192d0923df8e6ed8946ac
wfy <3381653964@qq.com>
2025-04-01 23:39:37 +0800

    林玉婷添加代码

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Sidebar from './Sidebar';
 import Hhhha from './Wfy/Hhhha';
 import Hhhh from './Hch/Hhhh';
 import Hhh from './Lhq/Hhh';
+import Hh from './Lyt/Hh';
 
 const App = () => {
   const menuItems = [
     { label: '韦凤莹', link: '/Hhhha' },
     { label: '黄城红', link: '/Hhhh' },
     { label: '刘慧琦', link: '/Hhh' },
+    { label: '林玉婷', link: '/Hh' },
   ];
   return (
     <Router>
@@ -19,6 +21,7 @@ const App = () => {
           <Route path="/Hhhha" element={<Hhhha />} />
           <Route path="/Hhhh" element={<Hhhh />} />
           <Route path="/Hhh" element={<Hhh />} />
+          <Route path="/Hh" element={<Hh />} />
         </Routes>
       </div>
     </Router>
