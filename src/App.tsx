9350faf1b6d0bf18436d127e123e11ceb9156e07
wfy <2307319669@qq.com>
2025-04-01 23:28:25 +0800

    刘慧琦添加代码

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Hhhha from './Wfy/Hhhha';
 import Hhhh from './Hch/Hhhh';
+import Hhh from './Lhq/Hhh';
 
 const App = () => {
   const menuItems = [
     { label: '韦凤莹', link: '/Hhhha' },
     { label: '黄城红', link: '/Hhhh' },
+    { label: '刘慧琦', link: '/Hhh' },
   ];
   return (
     <Router>
@@ -16,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/Hhhha" element={<Hhhha />} />
           <Route path="/Hhhh" element={<Hhhh />} />
+          <Route path="/Hhh" element={<Hhh />} />
         </Routes>
       </div>
     </Router>
